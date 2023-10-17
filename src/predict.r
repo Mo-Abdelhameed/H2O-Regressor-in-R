ab666eac8cae4be82c551eaeaaa65026cc7fb845
moo3030 <mohamedabdelhamid3030@gmail.com>
2023-10-17 15:44:22 +0300

    first commit

---
+++ src/predict.r
@@ -0,0 +1,58 @@
+library(jsonlite)
+library(h2o)
+
+
+# Paths
+ROOT_DIR <- dirname(getwd())
+MODEL_INPUTS_OUTPUTS <- file.path(ROOT_DIR, 'model_inputs_outputs')
+INPUT_DIR <- file.path(MODEL_INPUTS_OUTPUTS, "inputs")
+OUTPUT_DIR <- file.path(MODEL_INPUTS_OUTPUTS, "outputs")
+INPUT_SCHEMA_DIR <- file.path(INPUT_DIR, "schema")
+DATA_DIR <- file.path(INPUT_DIR, "data")
+TRAIN_DIR <- file.path(DATA_DIR, "training")
+TEST_DIR <- file.path(DATA_DIR, "testing")
+MODEL_ARTIFACTS_PATH <- file.path(MODEL_INPUTS_OUTPUTS, "model", "artifacts")
+PREDICTOR_FILE_PATH <- file.path(MODEL_ARTIFACTS_PATH, "predictor", "predictor.rds")
+PREDICTIONS_DIR <- file.path(OUTPUT_DIR, 'predictions')
+PREDICTIONS_FILE <- file.path(PREDICTIONS_DIR, 'predictions.csv')
+
+
+if (!dir.exists(PREDICTIONS_DIR)) {
+  dir.create(PREDICTIONS_DIR, recursive = TRUE)
+}
+
+# Reading the schema
+file_name <- list.files(INPUT_SCHEMA_DIR, pattern = "*.json")[1]
+schema <- fromJSON(file.path(INPUT_SCHEMA_DIR, file_name))
+features <- schema$features
+
+numeric_features <- features$name[features$dataType != 'CATEGORICAL']
+categorical_features <- features$name[features$dataType == 'CATEGORICAL']
+id_feature <- schema$id$name
+target_feature <- schema$target$name
+target_classes <- schema$target$classes
+model_category <- schema$modelCategory
+nullable_features <- features$name[features$nullable == TRUE]
+
+
+# Reading test data.
+file_name <- list.files(TEST_DIR, pattern = "*.csv", full.names = TRUE)[1]
+# Read the first line to get column names
+header_line <- readLines(file_name, n = 1)
+col_names <- unlist(strsplit(header_line, split = ",")) # assuming ',' is the delimiter
+# Read the CSV with the exact column names
+df <- read.csv(file_name, skip = 0, col.names = col_names, check.names=FALSE)
+
+ids <- df[[id_feature]]
+
+
+
+
+
+model <- readRDS(PREDICTOR_FILE_PATH)
+predictions <- h2o.predict(model, newdata = as.h2o(df))
+predictions_df <- as.data.frame(predictions)
+predictions_df[id_feature] <- ids
+colnames(predictions_df)[colnames(predictions_df) == "predict"] <- "prediction"
+
+write.csv(predictions_df, PREDICTIONS_FILE, row.names = FALSE)
---
+++ src/train.r
@@ -0,0 +1,70 @@
+# Required Libraries
+library(jsonlite)
+library(h2o)
+
+h2o.init()
+
+# Define directories and paths
+
+ROOT_DIR <- dirname(getwd())
+MODEL_INPUTS_OUTPUTS <- file.path(ROOT_DIR, 'model_inputs_outputs')
+INPUT_DIR <- file.path(MODEL_INPUTS_OUTPUTS, "inputs")
+INPUT_SCHEMA_DIR <- file.path(INPUT_DIR, "schema")
+DATA_DIR <- file.path(INPUT_DIR, "data")
+TRAIN_DIR <- file.path(DATA_DIR, "training")
+MODEL_ARTIFACTS_PATH <- file.path(MODEL_INPUTS_OUTPUTS, "model", "artifacts")
+PREDICTOR_FILE_PATH <- file.path(MODEL_ARTIFACTS_PATH, "predictor", "predictor.rds")
+
+
+if (!dir.exists(MODEL_ARTIFACTS_PATH)) {
+    dir.create(MODEL_ARTIFACTS_PATH, recursive = TRUE)
+}
+if (!dir.exists(file.path(MODEL_ARTIFACTS_PATH, "predictor"))) {
+    dir.create(file.path(MODEL_ARTIFACTS_PATH, "predictor"))
+}
+
+
+# Reading the schema
+# The schema contains metadata about the datasets. 
+# We will use the scehma to get information about the type of each feature (NUMERIC or CATEGORICAL)
+# and the id and target features, this will be helpful in preprocessing stage.
+
+file_name <- list.files(INPUT_SCHEMA_DIR, pattern = "*.json")[1]
+schema <- fromJSON(file.path(INPUT_SCHEMA_DIR, file_name))
+features <- schema$features
+
+numeric_features <- features$name[features$dataType == "NUMERIC"]
+categorical_features <- features$name[features$dataType == "CATEGORICAL"]
+id_feature <- schema$id$name
+target_feature <- schema$target$name
+model_category <- schema$modelCategory
+nullable_features <- features$name[features$nullable == TRUE]
+
+# Reading training data
+file_name <- list.files(TRAIN_DIR, pattern = "*.csv")[1]
+# Read the first line to get column names
+header_line <- readLines(file.path(TRAIN_DIR, file_name), n = 1)
+col_names <- unlist(strsplit(header_line, split = ",")) # assuming ',' is the delimiter
+# Read the CSV with the exact column names
+df <- read.csv(file.path(TRAIN_DIR, file_name), skip = 0, col.names = col_names, check.names=FALSE)
+
+splits <- h2o.splitFrame(as.h2o(df), ratios = 0.95, seed = 1234)
+train <- splits[[1]]
+test <- splits[[2]]
+
+
+automl_models <- h2o.automl(
+  x = features$name,
+  y = target_feature,
+  training_frame = train,
+  leaderboard_frame = test,
+  max_runtime_secs = 60,  # Set a maximum run time in seconds
+  project_name = "automl_regression"
+)
+
+leaderboard <- automl_models@leaderboard
+print(leaderboard)
+
+best_model <- automl_models@leader
+saveRDS(best_model, PREDICTOR_FILE_PATH)
+
